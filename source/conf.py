f750b666af1b532f551cebe8b3da52555ad719f4
Alex Band <alex@nlnetlabs.nl>
2021-04-24 18:13:35 +0200

    Dark mode

--- source/conf.py
+++ source/conf.py
@@ -16,6 +16,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -48,7 +49,7 @@ extensions = [
 numfig = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['../templates']
+# templates_path = ['../templates']
 
 locale_dirs = ['locale/']   #path is example but recommended.
 gettext_compact = False     #optional.
@@ -84,6 +85,7 @@ pygments_style = None
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -97,7 +99,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../static']
+html_static_path = ['resources']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -211,6 +213,8 @@ def setup(app):
                         objname='module sub-command',
                         indextemplate='pair: %s; module sub-command',
                         parse_node=parse_cmd_args_node)
+    app.add_css_file('css/dark.css')
+    app.add_css_file('css/light.css')
 
 # -- Options for extlinks extension
 
